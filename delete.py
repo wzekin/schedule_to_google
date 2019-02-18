10d62474bb21406f7d9db4c46b5b6ea1b79523ac
WangZekun <wzekin@gmail.com>
2019-02-18 18:12:36 +0800

    fix #1

---
+++ delete.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+from bs4 import BeautifulSoup
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+
+if __name__ == "__main__":
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+    lists = service.events().list(calendarId='primary').execute()
+    for i in lists['items']:
+        print(i)
+        service.events().delete(calendarId='primary',
+                                eventId=i['id']).execute()
--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def insert(summary, location, descripton, start_time, end_time):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
-    now = datetime.datetime.utcnow().isoformat() + '+08:00'  # indicates UTC+8 time
     service.events().insert(calendarId='primary', body={
         "end": {
             "dateTime": end_time
